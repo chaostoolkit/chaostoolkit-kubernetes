16960b987c502c9bcd104922c5e0ba37927a2aa4
Rodrigo Menezes <rodrigomenezes@pinterest.com>
2019-10-09 15:53:25 -0700

    fixes for microservice_available_and_healthy
    Signed-off-by: Rodrigo Menezes <rodrigomenezes@pinterest.com>

--- chaosk8s/probes.py
+++ chaosk8s/probes.py
@@ -49,24 +49,24 @@ def all_microservices_healthy(ns: str = "default",
 
 def microservice_available_and_healthy(
         name: str, ns: str = "default",
-        label_selector: str = "name in ({name})",
+        label_selector: str = None,
         secrets: Secrets = None) -> Union[bool, None]:
     """
     Lookup a deployment by `name` in the namespace `ns`.
-
     The selected resources are matched by the given `label_selector`.
-
     Raises :exc:`chaoslib.exceptions.ActivityFailed` when the state is not
     as expected.
     """
-    label_selector = label_selector.format(name=name)
+
+    field_selector = "metadata.name={name}".format(name=name)
     api = create_k8s_api_client(secrets)
 
     v1 = client.AppsV1beta1Api(api)
     if label_selector:
-        ret = v1.list_namespaced_deployment(ns, label_selector=label_selector)
+        ret = v1.list_namespaced_deployment(ns, field_selector=field_selector,
+                                            label_selector=label_selector)
     else:
-        ret = v1.list_namespaced_deployment(ns)
+        ret = v1.list_namespaced_deployment(ns, field_selector=field_selector)
 
     logger.debug("Found {d} deployment(s) named '{n}' in ns '{s}'".format(
         d=len(ret.items), n=name, s=ns))
