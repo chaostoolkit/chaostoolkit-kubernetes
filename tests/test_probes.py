fe94ca24d6cba86fd9aa38e797d50e9696154da0
enderv <chris.vodak@gmail.com>
2020-03-30 09:05:52 -0500

    Adding support for daemonset checks
    Signed-off-by: enderv <chris.vodak@gmail.com>

--- tests/test_probes.py
+++ tests/test_probes.py
@@ -7,6 +7,7 @@ import pytest
 import urllib3
 from chaoslib.exceptions import ActivityFailed
 
+from chaosk8s.daemonset.probes import daemonset_ready
 from chaosk8s.node.probes import get_nodes
 from chaosk8s.probes import all_microservices_healthy, \
     microservice_available_and_healthy, microservice_is_not_available, \
@@ -279,3 +280,60 @@ def test_can_select_nodes_by_label(cl, client, has_conf):
     v1.list_node.assert_called_with(
         label_selector=label_selector, _preload_content=False)
     assert resp == {"hey": "there"}
+
+
+@patch('chaosk8s.has_local_config_file', autospec=True)
+@patch('chaosk8s.daemonset.probes.client', autospec=True)
+@patch('chaosk8s.client')
+def test_daemonset_ready(cl, client, has_conf):
+    has_conf.return_value = False
+
+    ds = MagicMock()
+    ds.status.number_unavailable = None
+    ds.status.number_misscheduled = 0
+    result = MagicMock()
+    result.items = [ds,ds]
+
+    v1 = MagicMock()
+    v1.list_namespaced_daemon_set.return_value = result
+    client.AppsV1Api.return_value = v1
+
+    assert daemonset_ready("test")
+
+
+@patch('chaosk8s.has_local_config_file', autospec=True)
+@patch('chaosk8s.daemonset.probes.client', autospec=True)
+@patch('chaosk8s.client')
+def test_daemonset_not_ready(cl, client, has_conf):
+    has_conf.return_value = False
+
+    ds = MagicMock()
+    ds.status.number_unavailable = 1
+    ds.status.number_misscheduled = 0
+    result = MagicMock()
+    result.items = [ds,ds]
+
+    v1 = MagicMock()
+    v1.list_namespaced_daemon_set.return_value = result
+    client.AppsV1Api.return_value = v1
+
+    with pytest.raises(ActivityFailed) as excinfo:
+        daemonset_ready("test")
+    assert "DaemonSet has '1' unavailable replicas and '0' misscheduled" in str(excinfo.value)
+
+@patch('chaosk8s.has_local_config_file', autospec=True)
+@patch('chaosk8s.daemonset.probes.client', autospec=True)
+@patch('chaosk8s.client')
+def test_daemonset_ready_not_found(cl, client, has_conf):
+    has_conf.return_value = False
+    result = MagicMock()
+    result.items = []
+
+    v1 = MagicMock()
+    v1.list_namespaced_daemon_set.return_value = result
+    client.AppsV1Api.return_value = v1
+
+    name = "test"
+    with pytest.raises(ActivityFailed) as excinfo:
+        daemonset_ready(name)
+    assert "DaemonSet '{name}' was not found".format(name=name) in str(excinfo.value)
